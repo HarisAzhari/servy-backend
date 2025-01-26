b4b68139b0ef9d506146db2731d8093b508343ae
Haris Azhari <164475242+Matraxa535@users.noreply.github.com>
2025-01-26 22:22:47 +0800

    upload everything

--- main.py
+++ main.py
@@ -2234,4 +2234,4 @@ def get_recent_activities():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=8000, host='0.0.0.0')
